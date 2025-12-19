be25024e84b20925eacde9e04c89512515f45752
cherr <charity.grey1@gmail.com>
2025-12-18 16:43:04 -0800

    day 2 found bug

--- day2.cpp
+++ day2.cpp
@@ -5,44 +5,73 @@
 #include <array>
 #include <string>
 #include <sstream>
+#include <set>
 using namespace std;
+
+std::string repeat_substring(const std::string& sub, int n) {
+    std::string result = "";
+    for (int i = 0; i < n; ++i) {
+        result += sub;
+    }
+    return result;
+}
 
 // inclusive [start, end]
 long calculateInvalidSumB(string start, string end) {
-    int n_half = start.length() / 2;
-    int half_start;
+    int n = start.length();
+    long starti = stol(start);
+    long endi = stol(end);
 
-    if (start.length() % 2 == 1) {
-        half_start = stoi("1" + string(n_half, '0')); // ceil even number of digits
-    } else {
-        half_start = stoi(start.substr (0, n_half));
-        if (stoi(start.substr (n_half, n_half)) > half_start) {
-            half_start = half_start + 1;
-        }
-    }
+    int max_digits = end.length() % 2 == 0 ? end.length() / 2 : (end.length() / 2) + 1;
 
-    n_half = end.length() / 2;
-    int half_end;
-    if (end.length() % 2 == 1) {
-        half_end = stoi(string(n_half, '9')); // todo for floor even number of digits
-    } else {
-        half_end = stoi(end.substr (0, n_half));
-        if (stoi(end.substr (n_half, n_half)) < half_end) {
-            half_end = half_end - 1;
-        }
-    }
     long sum = 0;
-    for (int i = half_start; i <= half_end; i++) {
-        string first_half = to_string(i);
-        string second_half = first_half;
-        string full_number = first_half + second_half;
-        long full_num_int = stol(full_number);
-        if (full_num_int < stol(start) && full_num_int > stol(end)) {
-            cout << "Error in bounds calculation! Number:" << full_num_int << endl;
+    set<string> seen;
+    for (long i = 1; i <= max_digits; i++) {
+        // continue if digits dont divide into
+        if (n % i != 0) {
+            continue;
         }
-        sum += full_num_int;
-        // cout << "Adding number: " << full_num_int << endl;
+        int substr_start;
+        if (//(curr i = curr digit loop) > half of start digits 
+            i > (start.length() / 2)
+            ) {
+            substr_start = stoi("1" + string(i - 1, '0'));
+        } else {
+            substr_start = stoi(start.substr (0, i));
+            if (stol(repeat_substring(to_string(substr_start), n / i)) < starti) {
+                substr_start = substr_start + 1;
+            }
+        }
+
+        int substr_end; 
+        if (
+            //end digits > i digits
+            max_digits > i
+            ) {
+            substr_end = stoi(string(i, '9'));
+        } else {
+            substr_end = stoi(end.substr (0, i));
+            if (stol(repeat_substring(to_string(substr_end), n / i)) > endi) {
+                substr_end = substr_end - 1;
+            }
+        }
+
+        cout << "Checking substrings of length " << i << " from " << substr_start << " to " << substr_end << endl;
+        for (int j = substr_start; j <= substr_end; j++) {
+            string sub_str = to_string(j);
+            string full_number = repeat_substring(sub_str, n / i);
+            if (seen.find(full_number) != seen.end()) {
+                continue;
+            }
+            seen.insert(full_number);
+            long full_num_int = stol(full_number);
+            if (full_num_int >= starti && full_num_int <= endi) {
+                sum += full_num_int;
+                cout << "Adding number: " << full_num_int << endl;   
+            }
+        }
     }
+
     return sum;
 }
 
@@ -70,6 +99,8 @@ long calculateInvalidSumA(string start, string end) {
             half_end = half_end - 1;
         }
     }
+
+    
     long sum = 0;
     for (int i = half_start; i <= half_end; i++) {
         string first_half = to_string(i);
@@ -80,7 +111,7 @@ long calculateInvalidSumA(string start, string end) {
             cout << "Error in bounds calculation! Number:" << full_num_int << endl;
         }
         sum += full_num_int;
-        // cout << "Adding number: " << full_num_int << endl;
+        cout << "Adding number: " << full_num_int << endl;
     }
     return sum;
 }
@@ -118,5 +149,5 @@ int main(int argc, char** argv) {
     }
     
     cout << "Part B: " << invalid_sum << endl;
-    return 1;
+    return 0;
 }
