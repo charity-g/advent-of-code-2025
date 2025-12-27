c2d7490c05667603699476ae042aa237d4c5868e
cherr <charity.grey1@gmail.com>
2025-12-27 10:57:53 -0700

    failed pratb parsing

--- day6.cpp
+++ day6.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <execution>
 #include <numeric>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 void print(const vector<char>& column_types, const vector<vector<long>>& columns) {
@@ -19,7 +21,113 @@ void print(const vector<char>& column_types, const vector<vector<long>>& columns
     }
 }
 
-void partA(const vector<char>& column_types, const vector<vector<long>>& columns) {
+
+pair<vector<char>, vector<vector<long>>> partAParsing(ifstream& infile) {
+    vector<char> column_types;
+    vector<vector<long>> columns;
+    string line;
+    
+    getline(infile, line);
+    int i = 0;
+    while (i < line.size()) {
+            int start_i = i;
+            while (i < line.size() && line[i] != ' ') {
+                i++;
+            }
+            // cout << "column=" << line.substr(start_i, i - start_i) << endl;
+            vector<long> column;
+            column.push_back(stol(line.substr(start_i, i - start_i)));
+            columns.push_back(column);
+            i++;
+            while (i < line.size() && line[i] == ' ') {
+                i++;
+            }
+    }
+
+    while (getline(infile, line)) {
+        int col_i = 0;
+        i = 0;
+        while (i < line.size()) {
+            int start_i = i;
+            while (i < line.size() && line[i] != ' ') {
+                i++;
+            }
+            // cout << "column=" << line.substr(start_i, i - start_i) << endl;
+            try {
+                columns[col_i].push_back(stol(line.substr(start_i, i - start_i)));   
+            } catch (const std::exception& e) {
+                column_types.push_back(line[start_i]);
+            }
+            i++;
+            while (i < line.size() && line[i] == ' ') {
+                i++;
+            }
+            col_i++;
+        }
+    }
+    return make_pair(column_types, columns);
+}
+
+
+string to_string_func(long v) {
+    return to_string(v);
+}
+
+pair<vector<char>, vector<vector<long>>> partBParsing(ifstream& infile) {
+    vector<char> column_types;
+    vector<vector<string>> columns;
+    string line;
+    
+    getline(infile, line);
+    int i = 0;
+    while (i < line.size()) {
+            int start_i = i;
+            while (i < line.size() && line[i] != ' ') {
+                i++;
+            }
+            vector<string> column;
+            column.push_back(line.substr(start_i, i - start_i));
+            columns.push_back(column);
+            i++;
+    }
+
+    while (getline(infile, line)) {
+        if (!line.empty() && (line[0] == '+' || line[0] == '*')) {
+            break;
+        }
+        int col_i = 0;
+        i = 0;
+        while (i < line.size()) {
+            int col_len = columns[col_i][0].length();
+            cout << "col_i=" << col_i << " i=" << i << '-' << line.substr(i, col_len) << endl;
+            columns[col_i].push_back(line.substr(i, col_len));
+            i += col_len + 1;
+            col_i++;
+        }
+    }
+
+    i = 0;   
+    while (i < line.size()) {
+        if (line[i] != ' ') {
+            column_types.push_back(line[i]);
+        }
+        i++;
+    }
+
+    cout << "starting" << endl;
+    for (auto & col : columns) {
+        for (auto & val : col) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    vector<vector<long>> partb_columns;
+
+    return make_pair(column_types, partb_columns);
+}
+
+long calculation(const vector<char>& column_types, const vector<vector<long>>& columns) {
     long checksum = 0;
 
 
@@ -34,41 +142,26 @@ void partA(const vector<char>& column_types, const vector<vector<long>>& columns
         }
         checksum += curr_checksum;
     }
-    cout << "Part A has a checksum of " << checksum << endl;
+    return checksum;
+}
+
+
+void partA(ifstream& infile) {
+    auto [column_types, columns] = partAParsing(infile);
+    long result = calculation(column_types, columns);
+    cout << "Part A has a checksum of " << result << endl;
+}
+
+void partB(ifstream& infile) {
+    auto [column_types, columns] = partBParsing(infile);
+    long result = calculation(column_types, columns);
+    cout << "Part B has a checksum of " << result << endl;
 }
 
 
 int main(int argc, char** argv) {
     ifstream infile("inputs/day6.txt");
-
-    vector<char> column_types;
-    vector<vector<long>> columns;
-    string line;
+    partB(infile);
     
-    getline(infile, line);
-    for (int i = DEFAULT_COL_LEN - 1; i < line.size(); i += DEFAULT_COL_LEN + 1) {
-            // cout << "column=" << line.substr(i - DEFAULT_COL_LEN + 1, DEFAULT_COL_LEN) << endl;
-            vector<long> column;
-            column.push_back(stol(line.substr(i - DEFAULT_COL_LEN + 1, DEFAULT_COL_LEN)));
-            columns.push_back(column);
-    }
-
-    while (getline(infile, line)) {
-        int col_i = 0;
-        for (int i = DEFAULT_COL_LEN - 1; i < line.size(); i += DEFAULT_COL_LEN + 1) {
-            // cout << "column=" << line.substr(i - DEFAULT_COL_LEN + 1, DEFAULT_COL_LEN) << endl;
-            try {
-                columns[col_i].push_back(stol(line.substr(i - DEFAULT_COL_LEN + 1, DEFAULT_COL_LEN)));    
-            } catch (const std::exception& e) {
-                column_types.push_back(line[i - DEFAULT_COL_LEN + 1]);
-            }
-            col_i++;
-        }
-
-    }
-    
-    // print(column_types, columns);
-    partA(column_types, columns);
-
     return 0;
 }
