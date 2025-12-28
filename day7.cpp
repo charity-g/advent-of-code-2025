9cf80de29d502a4f07c303a70d7a32cb4deb95ad
cherr <charity.grey1@gmail.com>
2025-12-28 09:13:52 -0700

    add in day7 dp but not working for large example

--- day7.cpp
+++ day7.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <list>
 #include <set>
+#include <numeric>
 using namespace std;
 
 int findStartX(const string& line) {
@@ -37,6 +38,44 @@ void partA(ifstream& infile, int start) {
 }
 
 void partB(ifstream& infile, int start) {
+    vector<string> matrix;
+    string line;
+
+    while (getline(infile, line)) {
+        matrix.push_back(line);
+    }
+    
+    int n = matrix.size(), m = matrix[0].size();
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+
+    for (int i = 0; i < m; ++i) {
+        dp[n - 1][i] = 1;
+    }
+
+    for (int i = n - 2; i >= 0; --i) {
+        for (int j = 0; j < m; ++j) {
+            if (matrix[i][j] == '^') {
+                int total = 0;
+                if (j > 0) {
+                    total += dp[i + 1][j - 1];
+                } 
+                if (j < m - 1) {
+                    total += dp[i + 1][j + 1];
+                }
+                dp[i][j] = total;
+            } else {
+                dp[i][j] = dp[i + 1][j];
+            }
+        }
+    }
+
+    int timelines = dp[0][start];
+
+    cout << "Part B shows that there are: " << timelines << " timelines." << endl;
+}
+
+
+void partB_nonDP(ifstream& infile, int start) {
     int splits = 0;
     string line;
     list<int> q = {start};
